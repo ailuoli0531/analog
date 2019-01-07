4615e500e395aa5536aa50ce1c0c2dae666d48e3
bornic <wang1023>
2019-01-07 17:57:52 +0800

    更新字体

--- app/src/main/java/com/born/analog/AnaLogApplication.java
+++ app/src/main/java/com/born/analog/AnaLogApplication.java
@@ -1,8 +1,12 @@
 package com.born.analog;
 
 import android.app.Application;
+import android.graphics.Typeface;
 
 import com.born.analog.dao.DaoHelper;
+import com.born.analog.util.LogUtil;
+
+import java.lang.reflect.Field;
 
 /**
  * created by born on 2018/12/13.
@@ -13,5 +17,20 @@ public class AnaLogApplication extends Application {
         super.onCreate();
         //初始化数据库
         DaoHelper.init(this);
+        setTypeFace();
+    }
+
+    /**
+     * 设置全局字体
+     */
+    public void setTypeFace(){
+        Typeface typeface = Typeface.createFromAsset(getAssets(),"fonts/GirlType.ttf");
+        try {
+            Field field = Typeface.class.getDeclaredField("MONOSPACE");
+            field.setAccessible(true);
+            field.set(null, typeface);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
